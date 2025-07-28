16675b13f1e31483e33407edb1445a389a37010e
Lasini Pallewaththa <lasinipallewaththa.official@gmail.com>
2025-07-29 00:12:26 +0530

    tailsind fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/*.component.html",
+    "./src/**/*.component.ts"
+  ],
   theme: {
     extend: {},
   },
