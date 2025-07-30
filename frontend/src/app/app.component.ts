287248d88b61390ebe7b752646a2730cb637a346
Lasini Pallewaththa <lasinipallewaththa.official@gmail.com>
2025-07-31 00:53:46 +0530

    warnings fixed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingComponent } from "./components/landing/landing.component";
-import { ResultComponent } from "./components/result/result.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent, ResultComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
