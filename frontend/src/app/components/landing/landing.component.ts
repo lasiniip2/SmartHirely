73ce86b7ac54f8667a7bf25b6a0368c2ff5021c0
Lasini Pallewaththa <lasinipallewaththa.official@gmail.com>
2025-07-30 23:23:51 +0530

    nav and footer added

--- frontend/src/app/components/landing/landing.component.ts
+++ frontend/src/app/components/landing/landing.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from "../navigation/navigation.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [NavigationComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
